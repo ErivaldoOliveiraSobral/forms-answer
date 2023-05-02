1a936a11f329a50c906ab01145a01b07cbe723c4
Erivaldo Sobral <ice_esobral@uolinc.com>
2023-05-01 21:36:12 -0300

    Adicionando delay

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const { Selector } = require("testcafe");
 const { links } = require('./constants')
 
-const randomIntFromInterval = (min, max) => { 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-const answerAmount = 20;
+const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+const delay = minutes => new Promise(resolve => setTimeout(() => resolve(minutes), minutes * 60000));
+
+const answerAmount = 5;
 
 fixture`Bot de respostas`
 
@@ -12,9 +13,11 @@ let count = 0
 
 while (count < answerAmount) {
     const index = randomIntFromInterval(0, links.length - 1)
+    const waiting = randomIntFromInterval(1, 20);
     count++;
     
-    test(`acessa o link ${index} e clica em Enviar - Contagem ${count}`, async t => {
+    test(`aguarda ${waiting} minutos, acessa o link ${index} e clica em Enviar - Contagem ${count}`, async t => {
+        await delay(waiting);
         await t.navigateTo(links[index]).click(Selector('span').withExactText('Enviar'));
         await t.expect(Selector('div').withExactText('A sua resposta foi registada').exists).ok();
     });
